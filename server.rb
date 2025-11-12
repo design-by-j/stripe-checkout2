e63c4427273df675555d2e14865900aeea43436c
Charlie Svedevi <charlie.svedevi@gmail.com>
2025-11-12 01:00:04 +0100

    Uppdaterade success/cancel URLs för live deploy

--- server.rb
+++ server.rb
@@ -1,7 +1,20 @@
 require "sinatra"
 require "stripe"
+require "sinatra/cross_origin"
 require 'dotenv/load'
 require "json"
+
+configure do
+  enable :cross_origin
+end
+
+options "*" do
+  response.headers["Allow"] = "GET, POST, OPTIONS"
+  response.headers["Access-Control-Allow-Origin"] = "*"
+  response.headers["Access-Control-Allow-Methods"] = "GET, POST, OPTIONS"
+  response.headers["Access-Control-Allow-Headers"] = "Content-Type"
+  200
+end
 
 # Din Stripe Secret Key (test)
 Stripe.api_key = ENV['STRIPE_SECRET_KEY']
@@ -11,6 +24,7 @@ set :public_folder, "public"
 
 post "/create-checkout-session" do
   content_type :json
+headers "Access-Control-Allow-Origin" => "*"
 
   session = Stripe::Checkout::Session.create(
     payment_method_types: ["card"],
